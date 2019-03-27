22f94a8cf94759a4d25760a7482003555cdef6fe
StevenXPS13 <steven.parrott@gmail.com>
2019-03-27 10:22:05 -0700

    removed build foler from gitignore

---
+++ build/precache-manifest.9b11cae70719a6eae808b891285918fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "146a3181e76c06b70ec8",
+    "url": "/React-Memory-Game/static/js/runtime~main.39b1e4df.js"
+  },
+  {
+    "revision": "5478906242decf3bbf4a",
+    "url": "/React-Memory-Game/static/js/main.9d25be94.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/React-Memory-Game/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5478906242decf3bbf4a",
+    "url": "/React-Memory-Game/static/css/main.47185e7d.chunk.css"
+  },
+  {
+    "revision": "cb6f78a02638a026fc5ab7b170910de1",
+    "url": "/React-Memory-Game/index.html"
+  }
+];
